e889186f2437e0f2a95acb760d1b38b8e27adde6
Houssem Dellai <houssem.dellai.ext@geodis.com>
2018-01-18 22:22:02 +0100

    Add project files.

---
+++ InfiniteScrollingDemo/InfiniteScrollingDemo/DataService.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InfiniteScrollingDemo
+{
+    public class DataService
+    {
+        private readonly List<string> _data = new List<string>
+        {
+            "Mohamed", "Hassen", "Omar", "Ali", "Othman", "Adam", "Seif", "Hamed", "Paul",
+            "David", "Fehmi", "Badr", "Hamza", "Nabil", "Hajer", "Sami", "Ahmed", "Amir",
+            "Nebrass", "Karim", "Cherif", "Alaa", "Samar", "Narjess", "Iheb", "Safa",
+            "Mohamed", "Hassen", "Omar", "Ali", "Othman", "Adam", "Seif", "Hamed", "Paul",
+            "David", "Fehmi", "Badr", "Hamza", "Nabil", "Hajer", "Sami", "Ahmed", "Amir",
+        };
+
+        public async Task<List<string>> GetItemsAsync(int pageIndex, int pageSize)
+        {
+            await Task.Delay(2000);
+
+            return _data.Skip(pageIndex * pageSize).Take(pageSize).ToList();
+        }
+    }
+}
---
+++ InfiniteScrollingDemo/InfiniteScrollingDemo/MainViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Xamarin.Forms.Extended;
+
+namespace InfiniteScrollingDemo
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private bool _isBusy;
+        private const int PageSize = 10;
+
+        public InfiniteScrollCollection<string> Items { get; }
+
+        public bool IsBusy
+        {
+            get => _isBusy;
+            set
+            {
+                _isBusy = value; 
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            Items = new InfiniteScrollCollection<string>
+            {
+                OnLoadMore = async () =>
+                {
+                    IsBusy = true;
+                    
+                    // load the next page
+                    var page = Items.Count / PageSize;
+                    var dataService = new DataService();
+                    var items = await dataService.GetItemsAsync(page + 1, PageSize);
+
+                    IsBusy = false;
+
+                    // return the items that need to be added
+                    return items;
+                }
+            };
+
+            DownloadDataAsync();
+        }
+
+        private async Task DownloadDataAsync()
+        {
+            var dataService = new DataService();
+            var items = await dataService.GetItemsAsync(0, PageSize);
+            Items.AddRange(items);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
